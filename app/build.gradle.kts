44d7a2159391d60fd49b43c382c213eebf6c285d
NukumizuKazuhiko <noreply@github.com>
2026-08-18 16:59:19 +0800

    Bump version to 1.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,8 +37,8 @@ android {
     applicationId = "com.google.aiedge.gallery"
     minSdk = 31
     targetSdk = 37
-    versionCode = 39
-    versionName = "1.0.17"
+    versionCode = 40
+    versionName = "1.2.0"
 
     // Needed for HuggingFace auth workflows.
     // Use the scheme of the "Redirect URLs" in HuggingFace app.
